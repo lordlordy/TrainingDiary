3c9a948bca954b5c0bc40020dbb483317a7fefba
Steven Thomas Lord <stevenlord@me.com>
2019-09-24 10:38:40 +0100

    removed progress notification

--- trainingdiary/create_data_warehouse.py
+++ trainingdiary/create_data_warehouse.py
@@ -7,4 +7,4 @@ from workoutentry.data_warehouse import DataWarehouseGenerator
 
 if __name__ == '__main__':
     generator = DataWarehouseGenerator("training_data_warehouse.sqlite3")
-    generator.generate(print_progress=True)
+    generator.generate(print_progress=False)
